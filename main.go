04002a70947e62486ac682d5719f634cb335b11e
Igor <horror_int@yahoo.com>
2020-09-10 13:28:26 +0300

    Minor output changes

--- main.go
+++ main.go
@@ -9,71 +9,81 @@ import (
 )
 
 const (
-	helpString              = "Parameters: amount from_currency to_currency\nExample: fiatconv 123.45 USD RUB"
-	reqestFormat            = "https://api.exchangeratesapi.io/latest"
-	errConnStatusMessage    = "Connection status error"
-	errParsingJSONMessage   = "Error parsing JSON"
-	errPatsingCurrency      = "No value for"
-	errParsingAmountMessage = "Error parsing amount"
-	resultMessage           = "According to European Central Bank %6.4f %s = %6.4f %s for %s\n"
-	reachManMessage         = "%6.4f %v! Wow, you are rich man!\n"
-	looserManMessage        = "Only %6.4f %v?! Loser!\n"
+	helpString                = "Usage: fiatconv.exe amount from_currency to_currency\nExample: fiatconv 123.45 USD RUB"
+	reqestFormat              = "https://api.exchangeratesapi.io/latest"
+	errConnStatusMessage      = "Connection status error"
+	errParsingJSONMessage     = "Error parsing JSON"
+	errParsingCurrency        = "Currency %s not available\n"
+	errParsingAmountMessage   = "Error parsing amount"
+	resultMessage             = "According to European Central Bank %6.4f %s = %6.4f %s for %s\n"
+	reachManMessage           = "%6.4f %v! Wow, you are rich man!\n"
+	looserManMessage          = "Only %6.4f %v?! Loser!\n"
+	availableCurruciesMessage = "Available currencies:\n"
 )
+
+func printAvailabeCurrency(currencies *map[string]interface{}) {
+	fmt.Print(availableCurruciesMessage)
+	for currencyName, _ := range *currencies {
+		fmt.Fprintln(os.Stdout, currencyName)
+	}
+}
 
 func main() {
 	args := os.Args[1:]
 	if len(args) != 3 {
-		fmt.Println(helpString)
+		fmt.Fprintln(os.Stdout, helpString)
 		return
 	}
 
 	responce, err := http.Get(reqestFormat)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stdout, err)
 		return
 	}
 	if responce.StatusCode != http.StatusOK {
-		fmt.Println(errConnStatusMessage, responce.StatusCode)
+		fmt.Fprintln(os.Stdout, errConnStatusMessage, responce.StatusCode)
 		return
 	}
 
 	var data interface{}
 	if err := json.NewDecoder(responce.Body).Decode(&data); err != nil {
-		fmt.Println(errParsingJSONMessage, err)
+		fmt.Fprintln(os.Stdout, errParsingJSONMessage, err)
 		return
 	}
 
-	mainData := data.(map[string]interface{})
-	publishDate := mainData["date"].(string)
-	currencyData := mainData["rates"].(map[string]interface{})
+	rootData := data.(map[string]interface{})
+	publishDate := rootData["date"].(string)
+	currencyData := rootData["rates"].(map[string]interface{})
 
 	fromValue := currencyData[args[1]]
 	if fromValue == nil {
-		fmt.Println(errPatsingCurrency, args[1])
+		fmt.Fprintf(os.Stdout, errParsingCurrency, args[1])
+		printAvailabeCurrency(&currencyData)
 		return
 	}
 
 	toValue := currencyData[args[2]]
 	if toValue == nil {
-		fmt.Println(errPatsingCurrency, args[2])
+		fmt.Fprintf(os.Stdout, errParsingCurrency, args[2])
+		printAvailabeCurrency(&currencyData)
 		return
 	}
 
 	amount, err := strconv.ParseFloat(args[0], 64)
 	if err != nil {
-		fmt.Println(errParsingAmountMessage, args[0])
+		fmt.Fprintln(os.Stdout, errParsingAmountMessage, args[0])
 		return
 	}
 
 	convertionResult := amount / fromValue.(float64) * toValue.(float64)
 
 	if amount > 100 {
-		fmt.Printf(reachManMessage, amount, args[1])
+		fmt.Fprintf(os.Stdout, reachManMessage, amount, args[1])
 	}
 
 	if amount < 10 {
-		fmt.Printf(looserManMessage, amount, args[1])
+		fmt.Fprintf(os.Stdout, looserManMessage, amount, args[1])
 	}
 
-	fmt.Printf(resultMessage, amount, args[1], convertionResult, args[2], publishDate)
+	fmt.Fprintf(os.Stdout, resultMessage, amount, args[1], convertionResult, args[2], publishDate)
 }
