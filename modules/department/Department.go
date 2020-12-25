65e3153d59e21e10213b70c722c4965d10ea2d63
ThisIgor <horror_int@yahoo.com>
2020-12-25 19:33:18 +0300

    Update Department.go

--- modules/department/Department.go
+++ modules/department/Department.go
@@ -2,8 +2,10 @@ package department
 
 import "github.com/jinzhu/gorm"
 
+@startuml
 type Department struct {
 	gorm.Model
 	//.. Simple Getter ..
 	Name string `gorm:"type:varchar(1024);unique;not null"` //.. Simple Getter 1 ..
 }
+@enduml
