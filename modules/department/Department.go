b80ac9b3c9eb4c16e0af5e0618d11aeaca9ec645
ThisIgor <horror_int@yahoo.com>
2020-12-25 13:43:59 +0300

    Update Department.go

--- modules/department/Department.go
+++ modules/department/Department.go
@@ -4,6 +4,6 @@ import "github.com/jinzhu/gorm"
 
 type Department struct {
 	gorm.Model
-	// 1
-	Name string `gorm:"type:varchar(1024);unique;not null"` // 2
+	/* 1 */
+	Name string `gorm:"type:varchar(1024);unique;not null"` /* 2 */
 }
